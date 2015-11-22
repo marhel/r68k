0c23bef215dd31feebfe11bed97cc9c46b9cfbc4
Martin Hellspong <martin.hellspong@factor10.com>
2015-11-22 12:43:33 +0100

    Output simple CPU state

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,23 @@
 mod cpu;
 
+use std::io::Result;
+use std::io::prelude::*;
+use std::fs::File;
+
+
 fn main() {
-    let cpu = cpu::Core::new(64);
-    println!("Hello, CPU at {}", cpu.pc);
+	let cpu = cpu::Core::new(64);
+	println!("Hello, CPU at {}", cpu.pc);
+	match write_data(&cpu) {
+		Ok(_) => return (),
+		Err(e) => panic!(e),
+	};
+}
+
+fn write_data(core: &cpu::Core) -> Result<()> {
+	let mut buffer = try!(File::create("cpustate.txt"));
+	try!(write!(buffer, "{:04x} {:04x} {:04x}", core.pc, core.sp, core.status_register()));
+	Ok(())
 }
 
 // cpu state (all registers, flags etc.)
@@ -10,7 +25,7 @@ fn main() {
 //      m68ki_build_opcode_table();
 //		m68ki_jump(0);
 //		REG_SP = m68ki_read_imm_32();
-//		REG_PC = m68ki_read_imm_32();	
+//		REG_PC = m68ki_read_imm_32();
 //		m68ki_jump(REG_PC);
 
 // execute instruction (modifies CPU state)
