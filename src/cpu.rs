ee4ec7a043eda4e39cd7fbbf8fa7aa2cca30b0cc
Martin Hellspong <martin.hellspong@factor10.com>
2015-11-23 18:55:58 +0100

    Remove explicit return
    Didn't properly understand the difference between a semicolon terminated
    statement, and an expression.

--- src/cpu.rs
+++ src/cpu.rs
@@ -244,11 +244,10 @@ impl Core {
 	pub fn read_imm_32(&mut self) -> u32 {
 		let b = self.pc as usize;
 		self.pc += 4;
-		return ((self.mem[b+0] as u32) << 24
-			|   (self.mem[b+1] as u32) << 16
-			|   (self.mem[b+2] as u32) <<  8
-			|   (self.mem[b+3] as u32) <<  0
-			) as u32;
+		((self.mem[b+0] as u32) << 24
+		|(self.mem[b+1] as u32) << 16
+		|(self.mem[b+2] as u32) <<  8
+		|(self.mem[b+3] as u32) <<  0) as u32
 	}
 	pub fn read_imm_16(&mut self) -> u16 {
 		let b = self.pc as usize;
