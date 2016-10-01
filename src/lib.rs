68627d1583451ed89b5195d61237fd7e005c65fb
Martin Hellspong <martin.hellspong@factor10.com>
2016-10-01 23:22:10 +0200

    Made r68k a lib
    Added dasm and common (empty for now) as member crates
    Removed Cargo.lock

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+#[macro_use]
+extern crate lazy_static;
+extern crate itertools;
+
+pub mod cpu;
+pub mod ram;
+pub mod musashi;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+    }
+}
