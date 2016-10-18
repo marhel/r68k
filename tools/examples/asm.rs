c7c3fc2b06fdc093180f21c379786dfd7cd3cad9
Martin Hellspong <martin.hellspong@factor10.com>
2016-10-18 17:06:59 +0200

    Add trivial assembler example w/SRecord output

---
+++ tools/examples/asm.rs
@@ -0,0 +1,20 @@
+extern crate r68k_tools;
+
+use r68k_tools::assembler::Assembler;
+use std::io;
+use std::io::BufReader;
+use r68k_tools::srecords::write_s68;
+
+fn main() {
+    let r68k = Assembler::new();
+
+    let asm = r#"ADD.B   #$3,D0
+ADD.B   D0,D1"#;
+
+    println!("{}", asm);
+    let mut reader = BufReader::new(asm.as_bytes());
+    let (bytes, mem) = r68k.assemble(&mut reader).unwrap();
+    println!("wrote {} bytes", bytes);
+    let mut stdout = io::stdout();
+    write_s68(&mut stdout, vec![&mem], 0).unwrap();
+}
