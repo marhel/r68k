2fa3d8767cfd5a8f8228b4dc0f13afc07d1a5094
Martin Hellspong <martin.hellspong@factor10.com>
2017-01-03 23:36:39 +0100

    Add example integration of emu and assembler

---
+++ emu/examples/asm.rs
@@ -0,0 +1,31 @@
+extern crate r68k_emu;
+extern crate r68k_tools;
+
+use r68k_emu::cpu::Core;
+use r68k_tools::assembler::Assembler;
+use r68k_tools::memory::Memory;
+use std::io;
+use std::io::BufReader;
+use r68k_tools::srecords::write_s68;
+
+fn main() {
+    let r68k_asm = Assembler::new();
+
+    let asm = r#"
+    ; let's start off with a comment, and then set PC to $1000
+    ORG $1000
+
+    ADD.B   #$3,D0
+    ADD.B   D0,D1
+"#;
+
+    println!("{}", asm);
+    let mut reader = BufReader::new(asm.as_bytes());
+    let (end, mem) = r68k_asm.assemble(&mut reader).unwrap();
+    let offset = mem.offset();
+    let mut r68k_emu = Core::new_mem(offset, mem.data());
+    println!("assembled {:06x} - {:06x} and PC is {:06x}", offset, end, r68k_emu.pc);
+    let mut stdout = io::stdout();
+    write_s68(&mut stdout, vec![&mem], offset).unwrap();
+    r68k_emu.execute1();
+}
