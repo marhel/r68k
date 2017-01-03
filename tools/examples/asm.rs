79d4d2d905d8ecfbd9e52fd118b402dd0cdc04cd
Martin Hellspong <martin.hellspong@factor10.com>
2017-01-04 00:06:39 +0100

    Adapt asm example to new parser

--- tools/examples/asm.rs
+++ tools/examples/asm.rs
@@ -4,22 +4,24 @@ use r68k_tools::assembler::Assembler;
 use std::io;
 use std::io::BufReader;
 use r68k_tools::srecords::write_s68;
+use r68k_tools::memory::Memory;
 
 fn main() {
     let r68k = Assembler::new();
 
     let asm = r#"
-* let's start off with a comment, and then set PC to $1000
-ORG $1000
+    ; let's start off with a comment, and then set PC to $1000
+    ORG $1000
 
-ADD.B   #$3,D0
-ADD.B   D0,D1
+    ADD.B   #$3,D0
+    ADD.B   D0,D1
 "#;
 
     println!("{}", asm);
     let mut reader = BufReader::new(asm.as_bytes());
     let (end, mem) = r68k.assemble(&mut reader).unwrap();
-    println!("ends before address {:06x}", end);
+    let offset = mem.offset();
+    println!("assembled {:06x} - {:06x}", offset, end);
     let mut stdout = io::stdout();
     write_s68(&mut stdout, vec![&mem], 0).unwrap();
 }
