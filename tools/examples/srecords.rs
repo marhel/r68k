62b5deba941c5ee40e76422986c2d45c8ca8e01c
Martin Hellspong <martin.hellspong@factor10.com>
2018-07-27 16:15:07 +0200

    Fix example for PC newtype

--- tools/examples/srecords.rs
+++ tools/examples/srecords.rs
@@ -3,13 +3,14 @@ extern crate r68k_tools;
 use r68k_tools::srecords::write_s68;
 use std::io::LineWriter;
 use r68k_tools::memory::MemoryVec;
+use r68k_tools::PC;
 
 fn main() {
     let mut lw = LineWriter::new(vec![]);
 
     let data: Vec<u8> = (0u8 .. 0xFFu8).collect();
     let start = 0x2016;
-    let mem = MemoryVec::new8(start, data);
+    let mem = MemoryVec::new8(PC(start), data);
     write_s68(&mut lw, vec![&mem], start).unwrap();
 
     println!("{}", String::from_utf8(lw.into_inner().unwrap()).unwrap());
