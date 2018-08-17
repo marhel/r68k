57e8a13c9007fea54a55e21e32cf3a4bb4bf28d3
Martin Hellspong <martin.hellspong@factor10.com>
2018-08-17 13:47:56 +0200

    Switch instruction selection order for ADD/SUB/AND/OR
    A change in the is_ea_dn selector was made to support the instruction
    EOR Dn,Dn which also affected ADD/SUB/AND/OR.
    
    Using Dn,Dn needs to select <ea>,Dn mode for these instructions, instead
    of Dn,<ea>

--- tools/src/assembler/mod.rs
+++ tools/src/assembler/mod.rs
@@ -468,9 +468,9 @@ pub fn is_ea_dn(op: &OpcodeInstance) -> bool {
 }
 pub fn is_dn_ea(op: &OpcodeInstance) -> bool {
     if op.operands.len() != 2 { return false };
-    match op.operands[1] {
-        Operand::DataRegisterDirect(_) => false,
-        _ => true,
+    match op.operands[0] {
+        Operand::DataRegisterDirect(_) => true,
+        _ => false,
     }
 }
 pub fn is_imm_ea(op: &OpcodeInstance) -> bool {
